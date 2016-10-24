c9d88d31911510e24b483455e506d1cb4eca7c0b
Austin Blain and Jim Padilla <austin@>
2016-10-24 16:37:39 -0700

    Add review code to script

--- js/scripts.js
+++ js/scripts.js
@@ -4,12 +4,9 @@ var suits = ["Spades", "Clubs", "Diamonds", "Hearts"];
 var numbers = ["Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King"];
 
 $(document).ready(function() {
-debugger;
 suits.forEach(function(suit) {
-  debugger;
   suit = suits[suitCounter];
   numbers.forEach(function(number){
-    debugger;
     number = numbers[counter];
     $("#listHold").append("<li>" + numbers[counter] + " of " + suit + "</li>")
     counter ++;
@@ -19,3 +16,25 @@ suits.forEach(function(suit) {
 });
 
 });
+
+// var suits =["clubs", "diamonds", "spaces", "hearts"];
+// var faceValue =["ace", 2, 3, 4, 5, 6, 7, 8, 9, 10, "jack", "queen", "king"];
+// var cards = [];
+//
+// suits.forEach(function(suits){
+//   faceValue.forEach(function (value){
+//     cards.push(value + " of " + suits);
+//   });
+// });
+//
+//
+// $(document).ready(function() {
+//   $("form").submit(function(event) {
+//
+//      cards.forEach(function (card){
+//        $("ul").append("<li>" + card + "</li>");
+//      });
+//      $("#result").fadeIn();
+//     event.preventDefault();
+//   });
+// });
